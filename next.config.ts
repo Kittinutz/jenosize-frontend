bc9fc5b1ce28c75bb857d030edd1805ac9a1bf37
TonyDaoXYZ <tony@daoasis.xyz>
2026-02-19 04:10:58 +0700

    unoptimisation image

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: '*',
       },
     ],
   },
